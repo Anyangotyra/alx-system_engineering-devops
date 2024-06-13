c43ba95645c4f444460149e5e25615f1fea6ca25
Anyangotyra <tyrasophie@students.uonbi.ac.ke>
2024-06-13 07:00:20 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,2 @@
-# Fix web stack to accomodate a large amount of traffic
-
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart nginx
-
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
-}
+# Changes the OS configuration so that it is possible to login with the holberton user and open a file without any error message
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
